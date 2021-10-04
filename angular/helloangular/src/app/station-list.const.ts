aa18c12c4ccc2342f24ff68f96a7eb83f1b1eb3a
Csillag <sailorchien@gmail.com>
2021-10-04 22:06:14 +0800

    10_4

---
+++ angular/helloangular/src/app/station-list.const.ts
@@ -0,0 +1,162 @@
+export const stationList = [
+    {
+        'Station': '三和國中站',
+        'Destination': '南勢角站',
+        'UpdateTime': '2021-09-21T12:54:40.65'
+      },
+      {
+        'Station': '大安站',
+        'Destination': '南港展覽館站',
+        'UpdateTime': '2021-09-21T12:54:31'
+      },
+      {
+        'Station': '大湖公園站',
+        'Destination': '南港展覽館站',
+        'UpdateTime': '2021-09-21T12:54:31'
+      },
+      {
+        'Station': '中山國小站',
+        'Destination': '南勢角站',
+        'UpdateTime': '2021-09-21T12:54:06.59'
+      },
+      {
+        'Station': '中山國中站',
+        'Destination': '動物園站',
+        'UpdateTime': '2021-09-21T12:54:31'
+      },
+      {
+        'Station': '中正紀念堂站',
+        'Destination': '松山站',
+        'UpdateTime': '2021-09-21T12:54:32.37'
+      },
+      {
+        'Station': '中正紀念堂站',
+        'Destination': '淡水站',
+        'UpdateTime': '2021-09-21T12:54:14.42'
+      },
+      {
+        'Station': '丹鳳站',
+        'Destination': '迴龍站',
+        'UpdateTime': '2021-09-21T12:54:25.687'
+      },
+      {
+        'Station': '六張犁站',
+        'Destination': '南港展覽館站',
+        'UpdateTime': '2021-09-21T12:54:31'
+      },
+      {
+        'Station': '古亭站',
+        'Destination': '新店站',
+        'UpdateTime': '2021-09-21T12:54:32.37'
+      },
+      {
+        'Station': '先嗇宮站',
+        'Destination': '迴龍站',
+        'UpdateTime': '2021-09-21T12:54:25.687'
+      },
+      {
+        'Station': '西門站',
+        'Destination': '新店站',
+        'UpdateTime': '2021-09-21T12:54:32.37'
+      },
+      {
+        'Station': '西湖站',
+        'Destination': '動物園站',
+        'UpdateTime': '2021-09-21T12:54:31'
+      },
+      {
+        'Station': '辛亥站',
+        'Destination': '南港展覽館站',
+        'UpdateTime': '2021-09-21T12:54:31'
+      },
+      {
+        'Station': '忠孝復興站',
+        'Destination': '動物園站',
+        'UpdateTime': '2021-09-21T12:54:31'
+      },
+      {
+        'Station': '忠孝敦化站',
+        'Destination': '南港展覽館站',
+        'UpdateTime': '2021-09-21T12:54:26.923'
+      },
+      {
+        'Station': '忠孝新生站',
+        'Destination': '南勢角站',
+        'UpdateTime': '2021-09-21T12:54:40.65'
+      },
+      {
+        'Station': '明德站',
+        'Destination': '大安站',
+        'UpdateTime': '2021-09-21T12:54:34.75'
+      },
+      {
+        'Station': '東門站',
+        'Destination': '象山站',
+        'UpdateTime': '2021-09-21T12:54:14.42'
+      },
+      {
+        'Station': '東湖站',
+        'Destination': '南港展覽館站',
+        'UpdateTime': '2021-09-21T12:54:31'
+      },
+      {
+        'Station': '板橋站',
+        'Destination': '頂埔站',
+        'UpdateTime': '2021-09-21T12:54:26.923'
+      },
+      {
+        'Station': '松山機場站',
+        'Destination': '動物園站',
+        'UpdateTime': '2021-09-21T12:54:31'
+      },
+      {
+        'Station': '信義安和站',
+        'Destination': '淡水站',
+        'UpdateTime': '2021-09-21T12:54:14.42'
+      },
+      {
+        'Station': '後山埤站',
+        'Destination': '頂埔站',
+        'UpdateTime': '2021-09-21T12:54:37.183'
+      },
+      {
+        'Station': '淡水站',
+        'Destination': '象山站',
+        'UpdateTime': '2021-09-21T12:54:24.527'
+      },
+      {
+        'Station': '頂溪站',
+        'Destination': '迴龍站',
+        'UpdateTime': '2021-09-21T12:54:06.59'
+      },
+      {
+        'Station': '唭哩岸站',
+        'Destination': '北投站',
+        'UpdateTime': '2021-09-21T12:54:34.75'
+      },
+      {
+        'Station': '唭哩岸站',
+        'Destination': '象山站',
+        'UpdateTime': '2021-09-21T12:54:24.527'
+      },
+      {
+        'Station': '景安站',
+        'Destination': '蘆洲站',
+        'UpdateTime': '2021-09-21T12:54:20.13'
+      },
+      {
+        'Station': '景美站',
+        'Destination': '新店站',
+        'UpdateTime': '2021-09-21T12:54:32.37'
+      },
+      {
+        'Station': '新北產業園區站',
+        'Destination': '大坪林站',
+        'UpdateTime': '2021-09-21T12:54:36'
+      },
+      {
+        'Station': '萬芳醫院站',
+        'Destination': '動物園站',
+        'UpdateTime': '2021-09-21T12:54:31'
+      }
+];
