1e59077ffabf3d05c62a7ebf3e1d46ebb90d4dd7
Csillag <sailorchien@gmail.com>
2021-10-30 21:56:38 +0800

    gui

--- python/tkinter1.py
+++ python/tkinter1.py
@@ -3,8 +3,19 @@ import tkinter as tk
 window = tk.Tk()
 window.title('window')
 
-def create_label(txt):
-    lbl_1 = tk.Label(window, text=txt, bg='yellow', fg='#263238', font=('Arial', 12), width=100, height=10)
-    lbl_1.grid(column=0, row=0)
-create_label('Hello World !')
+def create_button(txt):
+    bt_1 = tk.Button(window, text=txt, bg='red', fg='white', font=('Arial', 12))
+    bt_1['width'] = 50
+    bt_1['height'] = 4
+    bt_1['activebackground'] = 'grey1'        
+# 按鈕被按下的背景顏色
+
+    bt_1['activeforeground'] = 'yellow'     
+# 按鈕被按下的文字顏色 ( 前景 )
+
+
+    bt_1.grid(column=0, row=0)
+
+create_button('Button')
+
 window.mainloop()
