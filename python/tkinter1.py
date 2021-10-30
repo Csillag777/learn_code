5c29b7dadd25efe94f76186b4780ccbf48cf1263
Csillag <sailorchien@gmail.com>
2021-10-31 00:04:04 +0800

    gui

--- python/tkinter1.py
+++ python/tkinter1.py
@@ -1,21 +1,35 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
+def define_layout(obj, cols=1, rows=1):
+    
+    def method(trg, col, row):
+        
+        for c in range(cols):    
+            trg.columnconfigure(c, weight=1)
+        for r in range(rows):
+            trg.rowconfigure(r, weight=1)
+
+    if type(obj)==list:        
+        [ method(trg, cols, rows) for trg in obj ]
+    else:
+        trg = obj
+        method(trg, cols, rows)
 window = tk.Tk()
-window.title('window')
+window.title('Window')
+align_mode = 'nswe'
+pad = 5
+#window.attributes('-fullscreen', True)
+div_size = 200
+img_size = div_size * 2
+div1 = tk.Frame(window,  width=img_size , height=img_size , bg='blue')
+div2 = tk.Frame(window,  width=div_size , height=div_size , bg='orange')
+div3 = tk.Frame(window,  width=div_size , height=div_size , bg='green')
 
-def create_button(txt):
-    bt_1 = tk.Button(window, text=txt, bg='red', fg='white', font=('Arial', 12))
-    bt_1['width'] = 50
-    bt_1['height'] = 4
-    bt_1['activebackground'] = 'grey1'        
-# 按鈕被按下的背景顏色
-
-    bt_1['activeforeground'] = 'yellow'     
-# 按鈕被按下的文字顏色 ( 前景 )
-
-
-    bt_1.grid(column=0, row=0)
-
-create_button('Button')
+div1.grid(column=0, row=0, padx=pad, pady=pad, rowspan=2, sticky=align_mode)
+div2.grid(column=1, row=0, padx=pad, pady=pad, sticky=align_mode)
+div3.grid(column=1, row=1, padx=pad, pady=pad, sticky=align_mode)
+define_layout(window, cols=2, rows=2)
+define_layout([div1, div2, div3])
 
 window.mainloop()
