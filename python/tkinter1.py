385b38508c2db70cc5027745fee802e14a658294
Csillag <sailorchien@gmail.com>
2021-10-30 21:53:08 +0800

    gui

--- python/tkinter1.py
+++ python/tkinter1.py
@@ -2,7 +2,9 @@ import tkinter as tk
 
 window = tk.Tk()
 window.title('window')
-window.geometry('500x100')
-lbl_1 = tk.Label(window, text='Hello World', bg='yellow', fg='#263238', font=('Arial', 12))
-lbl_1.grid(column=0, row=0)
+
+def create_label(txt):
+    lbl_1 = tk.Label(window, text=txt, bg='yellow', fg='#263238', font=('Arial', 12), width=100, height=10)
+    lbl_1.grid(column=0, row=0)
+create_label('Hello World !')
 window.mainloop()
