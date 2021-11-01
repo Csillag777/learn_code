6b53b09277851bcb0f296643f2c11c7b7fb0e336
Csillag <sailorchien@gmail.com>
2021-11-01 23:50:33 +0800

    11_1

--- python/3.py
+++ python/3.py
@@ -1,34 +1,55 @@
 import tkinter as tk
+from tkinter.constants import TOP
 from typing import Sized
 from PIL import Image, ImageTk
 import tkinter.font as tkFont
-
+from tkinter import ttk
+from tkinter import messagebox
 window = tk.Tk()
 window.title('dash borad test')
 window.geometry('800x600')
 window.configure(background='grey')
+
 align_mode = 'nswe'
 pad = 5
-fontStyle = tkFont.Font(family="Lucida Grande", size=10)
+fontStyle = tkFont.Font(family="Lucida Grande", size=20)
+
+def show():
+    
+    result = 'hello world'
+    result_label.configure(text=result)
+def alert():
+    messagebox.showinfo("Basic Example", "a Basic Tk MessageBox")
+
 
 fontStyle1 = tkFont.Font(family="Lucida Grande", size=20)
 div1 = tk.Label(window, text='Dash Board',bg="grey",font=fontStyle,fg='white')
-div2 = tk.Button(window, text="up",bg="green",font=fontStyle)
-div3 = tk.Button(window, text="down",bg="pink",font=fontStyle)
-div4 = tk.Button(window, text="left",bg="orange",font=fontStyle)
-div5 = tk.Button(window, text="right",bg="blue",font=fontStyle)
-div1.grid(column=0, row=0, padx=pad, pady=pad,sticky=align_mode)
-div2.grid(column=0, row=1, padx=pad, pady=pad, sticky=align_mode)
-div3.grid(column=1, row=1, padx=pad, pady=pad, sticky=align_mode)
-div4.grid(column=2, row=1, padx=pad, pady=pad, sticky=align_mode)
-div5.grid(column=3, row=1, padx=pad, pady=pad, sticky=align_mode)
-result_label = tk.Label(window, text='x = ',bg="grey",font=fontStyle,fg='white')
+div1.pack(side = tk.TOP)
+
+result_label = tk.Label(window,bg="grey",font=fontStyle,fg='white')
+result_label.pack()
 
 
-def move(x,y):
-    result = 'x={} y= {}'.format(x, y)
-    result_label.configure(text=result)
+div2 = tk.Button(window, text="hello",bg="orange",font=fontStyle,command=show)
+div2.pack(side = tk.TOP)
 
+labelTop = tk.Label(window,text = "Choose your favourite month",bg="grey",font=fontStyle,fg='white')
+labelTop.pack(side = tk.TOP)
 
+comboExample = ttk.Combobox(window, 
+                            values=[
+                                    "January", 
+                                    "February",
+                                    "March",
+                                    "April"])
+print(dict(comboExample)) 
+comboExample.pack(side = tk.TOP)
+comboExample.current(1)
+
+print(comboExample.current(), comboExample.get())
+
+div3 = tk.Button(window, text="alert",bg="yellow",command=alert)
+div3.pack(side = tk.TOP)
+#result_label.grid(column=0, row=2, padx=pad, pady=pad, sticky=align_mode)
 
 window.mainloop()
