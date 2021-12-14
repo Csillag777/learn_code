f1cdc980651840262a9f4f19f7fa8de9c39fd20a
Csillag <sailorchien@gmail.com>
2021-12-14 22:38:48 +0800

    12_14

--- python/plot2.py
+++ python/plot2.py
@@ -1,10 +1,7 @@
-from plotnine.data import economics
-from plotnine import ggplot, aes, scale_x_timedelta, labs, geom_line
-
-(
-    ggplot(economics)
-    + aes(x="date", y="pop")
-    + scale_x_timedelta(name="Years since 1970")
-    + labs(title="Population Evolution", y="Population")
-    + geom_line()
-)
+from plotnine import ggplot, geom_point, aes, stat_smooth, facet_wrap
+from plotnine.data import mtcars
+print(mpg)
+(ggplot(mtcars, aes('wt', 'mpg', color='factor(gear)'))
+ + geom_point()
+ + stat_smooth(method='lm')
+ + facet_wrap('~gear'))
